d3b7d1590764b1e11e89e85f7131386ec4fa5326
Ashley Brinson <m3ac-brinsona@m3as.com>
2021-11-19 11:10:27 -0500

    Completes module 6

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,11 +3,13 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
@@ -22,5 +24,9 @@ export class ProductService {
       };
       }
     )
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json())
   }
 }
